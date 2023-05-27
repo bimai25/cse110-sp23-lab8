982aa44fb7eae207acfe7d96db1240e1e903f124
bimai25 <bimai@ucsd.edu>
2023-05-27 00:50:46 -0700

    completed expose and explore

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Lab 8 - Starter
+**Question 1:** I would choose option 2 because the Recipe project is more of a personal project and it would be more efficient to check for bugs/do testing locally so that changes can be made faster. This Recipe project is not some sort of sub routine on a bigger project which would warrant testing via a GitHub Action. In other words there are not many pushes to GitHub overall so a GitHub Action seems unnecessary. Furthermore, we wouldn't want to run testing after development is finished because that can lead to headaches if a lot of test fails. We should constantly test to follow Agile.
+
+**Question 2:** No
+
+**Question 3:** I would not use a unit test to test the "messaging" feature because this process implies a user experience. This requires interaction with a webpage rather than testing an individual component. This seems more fit for an E2E test.
+
+**Question 4:** I would use a unit test to test this feature because this is something that can be compartamentalized and be executed quickly. It is a logic check, not something that requires user interaction.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,82 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+test('valid phone number #1', () => {
+    expect(functions.isPhoneNumber("123-456-7890")).toBe(true);
+});
+
+test('valid phone number #2', () => {
+    expect(functions.isPhoneNumber("(123) 456-7890")).toBe(true);
+});
+
+test('invalid phone number #1', () => {
+    expect(functions.isPhoneNumber("12-34-56")).toBe(false);
+});
+
+test('invalid phone number #2', () => {
+    expect(functions.isPhoneNumber("(123)-456-(7890)")).toBe(false);
+});
+
+test('valid email #1', () => {
+    expect(functions.isEmail("anybody@ucsd.edu")).toBe(true);
+});
+
+test('valid email #2', () => {
+    expect(functions.isEmail("england@uk.eu")).toBe(true);
+})
+
+test('invalid email #1', () => {
+    expect(functions.isEmail("this@domain@ucsd.edu")).toBe(false);
+});
+
+test('invalid email #2', () => {
+    expect(functions.isEmail("no email is here")).toBe(false);
+});
+
+test('valid password #1', () => {
+    expect(functions.isStrongPassword("a12_")).toBe(true);
+});
+
+test('valid password #2', () => {
+    expect(functions.isStrongPassword("a1234567890____")).toBe(true);
+});
+
+test('invalid password #1', () => {
+    expect(functions.isStrongPassword("1235")).toBe(false);
+});
+
+test('invalid password #2', () =>{
+    expect(functions.isStrongPassword("a6783?8g2")).toBe(false);
+});
+
+test('valid date #1', () => {
+    expect(functions.isDate("3/05/2004")).toBe(true);
+});
+
+test('valid date #2', () => {
+    expect(functions.isDate("03/5/2004")).toBe(true);
+});
+
+test('invalid date #1', () => {
+    expect(functions.isDate("03052004")).toBe(false);
+});
+
+test('invalid date #2', () => {
+    expect(functions.isDate("03/05/25")).toBe(false);
+});
+
+test('valid hexcode #1', () => {
+    expect(functions.isHexColor("#AA3")).toBe(true);
+});
+
+test('valid hexcode #2', () => {
+    expect(functions.isHexColor("#AAA333")).toBe(true);
+});
+
+test('invalid hexcode #1', () => {
+    expect(functions.isHexColor("#AFH")).toBe(false);
+});
+
+test('invalid hexcode #2', () => {
+    expect(functions.isHexColor("#AA%")).toBe(false);
+});
